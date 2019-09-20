cbf98298bdf9ea14a602ed092172f370bc925d78
go-jet <zer0.sub@protonmail.com>
2019-09-20 20:04:17 +0200

    Fix linter errors.

--- internal/jet/sql_builder.go
+++ internal/jet/sql_builder.go
@@ -142,27 +142,12 @@ func argToString(value interface{}) string {
 			return "TRUE"
 		}
 		return "FALSE"
-	case int8:
-		return strconv.FormatInt(int64(bindVal), 10)
 	case int:
-		return strconv.FormatInt(int64(bindVal), 10)
-	case int16:
 		return strconv.FormatInt(int64(bindVal), 10)
 	case int32:
 		return strconv.FormatInt(int64(bindVal), 10)
 	case int64:
 		return strconv.FormatInt(bindVal, 10)
-
-	case uint8:
-		return strconv.FormatUint(uint64(bindVal), 10)
-	case uint:
-		return strconv.FormatUint(uint64(bindVal), 10)
-	case uint16:
-		return strconv.FormatUint(uint64(bindVal), 10)
-	case uint32:
-		return strconv.FormatUint(uint64(bindVal), 10)
-	case uint64:
-		return strconv.FormatUint(uint64(bindVal), 10)
 
 	case float32:
 		return strconv.FormatFloat(float64(bindVal), 'f', -1, 64)
