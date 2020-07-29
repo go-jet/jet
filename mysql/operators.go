9a3c29b504dd3c5ac4f1fb2fb71f4701e9847f11
Joonas Haapsaari <joonas.haapsaari@gmail.com>
2020-07-29 16:37:55 +0300

    MySQL NOT operator support
    - Suppot for NOT operator in MySQL, similarly as in PostgreSQL.
    - Add test to verify NOT EXISTS query in MySQL

---
+++ mysql/operators.go
@@ -0,0 +1,9 @@
+package mysql
+
+import "github.com/go-jet/jet/v2/internal/jet"
+
+// NOT returns negation of bool expression result
+var NOT = jet.NOT
+
+// BIT_NOT inverts every bit in integer expression result
+var BIT_NOT = jet.BIT_NOT
