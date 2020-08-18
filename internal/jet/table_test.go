013e93a04edf5e1d25dc10e7f82fc31d6e7a5bed
Joonas Haapsaari <joonas.haapsaari@gmail.com>
2020-08-18 15:47:10 +0300

    Schema rename support
    - Support for renaming table schemas
     * Table support for renaming schema
     * Empty schema name is left out (using default schema for the
       database connection)
     * Generated code support for obtaining a version of the table with
       renamed schema, similarly as the `AS` function works
     * Unit tests for setting and clearing the schema name

--- internal/jet/table_test.go
+++ internal/jet/table_test.go
@@ -31,3 +31,38 @@ INNER JOIN schema.table2 ON ("intCol1" = "intCol2")`)
 	require.Equal(t, joinTable.columns()[0].Name(), "intCol1")
 	require.Equal(t, joinTable.columns()[1].Name(), "intCol2")
 }
+
+func TestSchemaNameSet(t *testing.T) {
+	newTable := NewTable("schema", "table")
+	newTable.Schema("foo")
+	newTable.AS("bar")
+	assertClauseSerialize(t, newTable, `foo.table AS bar`)
+}
+
+func TestSchemaNameClear(t *testing.T) {
+	newTable := NewTable("schema", "table")
+	newTable.Schema("")
+	newTable.AS("bar")
+	assertClauseSerialize(t, newTable, `table AS bar`)
+}
+
+func TestNewJoinTableSchemaNameSet(t *testing.T) {
+	newTable1 := NewTable("schema", "table", IntegerColumn("intCol1"))
+	newTable2 := NewTable("schema", "table2", IntegerColumn("intCol2"))
+
+	newTable1.Schema("foo")
+	newTable2.Schema("foo")
+
+	joinTable := NewJoinTable(newTable1, newTable2, InnerJoin, IntegerColumn("intCol1").EQ(IntegerColumn("intCol2")))
+	joinTable.Schema("xxx")
+
+	assertClauseSerialize(t, joinTable, `foo.table
+INNER JOIN foo.table2 ON ("intCol1" = "intCol2")`)
+
+	require.Equal(t, joinTable.SchemaName(), "foo")
+	require.Equal(t, joinTable.TableName(), "")
+
+	require.Equal(t, len(joinTable.columns()), 2)
+	require.Equal(t, joinTable.columns()[0].Name(), "intCol1")
+	require.Equal(t, joinTable.columns()[1].Name(), "intCol2")
+}
