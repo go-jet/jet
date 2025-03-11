77dc0bcb57413fc60cff31d5e96d596778c35695
SwitchUpCB <81384235+switchupcb@users.noreply.github.com>
2025-03-11 15:39:22 +0000

    remove unused
    https://github.com/go-jet/jet/pull/458#discussion_r1986128416

--- internal/utils/filesys/filesys.go
+++ internal/utils/filesys/filesys.go
@@ -56,24 +56,3 @@ func EnsureDirPathExist(dirPath string) error {
 
 	return nil
 }
-
-// RemoveDir deletes everything at folder dir.
-func RemoveDir(dir string) error {
-	if err := os.RemoveAll(dir); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// DirExists checks if folder at path exist.
-func DirExists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return true, err
-}
